3c39cfec2ea2ad1e480ddc8d2985e8ae0d06f023
MarkEhler <ehlerm42@gmail.com>
2019-09-19 14:12:39 -0400

    michael j fox show

--- app/routes.py
+++ app/routes.py
@@ -29,7 +29,7 @@ def handle_data():
     for i in range(7):
         session.pop(str(i), None)
     # session.pop('time', None)
-    listed, holdit, avgs = process(output, int(request.form['time_span']), sunrise, sunset)
+    listed, avgs = process(output, int(request.form['time_span']), sunrise, sunset)
     for idx, i in enumerate(listed):
         session[str(idx)] = i
         print(idx)
@@ -47,7 +47,7 @@ def plot_png():
 # option to hard code time index for plot
 # remeber to subtract one from len of session object for the time
 def create_figure(session_obj):
-    fig = Figure(figsize=(10,3*len(session_obj)))
+    fig = Figure(figsize=(10,4*len(session_obj)))
     times = ["00:00", "00:15", "00:30", "00:45", "01:00", "01:15", "01:30", "01:45", "02:00", "02:15", "02:30", "02:45", "03:00", "03:15", "03:30", "03:45", "04:00", "04:15", "04:30", "04:45", "05:00", "05:15", "05:30", "05:45", "06:00", "06:15", "06:30", "06:45", "07:00", "07:15", "07:30", "07:45", "08:00", "08:15", "08:30", "08:45", "09:00", "09:15", "09:30", "09:45", "10:00", "10:15", "10:30", "10:45", "11:00", "11:15", "11:30", "11:45", "12:00", "12:15", "12:30", "12:45", "13:00", "13:15", "13:30", "13:45", "14:00", "14:15", "14:30", "14:45", "15:00", "15:15", "15:30", "15:45", "16:00", "16:15", "16:30", "16:45", "17:00", "17:15", "17:30", "17:45", "18:00", "18:15", "18:30", "18:45", "19:00", "19:15", "19:30", "19:45", "20:00", "20:15", "20:30", "20:45", "21:00", "21:15", "21:30", "21:45", "22:00", "22:15", "22:30", "22:45", "23:00", "23:15", "23:30", "23:45"]
     # counter = 1
     print(times)
@@ -60,15 +60,15 @@ def create_figure(session_obj):
         axis = fig.add_subplot(1, 1, 1)
         axis.plot(times, day, label='Photovoltaic Energy Produced',
                 color='orange', fillstyle='bottom')
-        axis.set_xlabel('Time', fontdict = {'fontsize' : 20})
-        axis.set_ylabel('Watt per Square Meter of Panels', fontdict = {'fontsize' : 20})
+        axis.set_xlabel('Time', fontdict = {'fontsize' : 14})
+        axis.set_ylabel('Watts / Meter^2', fontdict = {'fontsize' : 14})
         axis.legend(loc='upper left')
-        axis.set_title(f'Day {idx+1}', fontdict = {'fontsize' : 24}, loc= 'left')
+        axis.set_title(f'Day {idx+1}', fontdict = {'fontsize' : 14}, loc= 'left')
         for tick in axis.xaxis.get_major_ticks():
             tick.label.set_fontsize(8) 
             tick.label.set_rotation(65)
         for tick in axis.yaxis.get_major_ticks():
-            tick.label.set_fontsize(22)
+            tick.label.set_fontsize(18)
         # counter += 1
     return fig
 
@@ -102,8 +102,6 @@ def process(final_data, days, sunrise, sunset):
     dayz = final_data.Day.unique()
     listed = []
     averages = []
-    times = list(final_data.index)
-    times = json.dumps([convert_minutes(time, seconds=False) for time in times[:96]])
     for day in dayz:
         day = final_data[final_data.Day == day]
         #         day[:sunrise_minutes] = 0
@@ -112,4 +110,4 @@ def process(final_data, days, sunrise, sunset):
         day = day['Output'].to_json()
         listed.append(day)
 
-    return listed, times, averages
+    return listed, averages
