e808ee7576f1eff89ccfb76fc55670750057fbcc
MarkEhler <ehlerm42@gmail.com>
2019-09-19 13:36:56 -0400

    stupid session time object

--- app/routes.py
+++ app/routes.py
@@ -27,8 +27,8 @@ def handle_data():
     output, sunrise, sunset = loop_data_collect(int(request.form['time_span']), request.form['location'], request.form['date'])
     for i in range(7):
         session.pop(str(i), None)
-    session.pop('time', None)
-    listed, session['time'], avgs = process(output, int(request.form['time_span']), sunrise, sunset)
+    # session.pop('time', None)
+    listed, holdit, avgs = process(output, int(request.form['time_span']), sunrise, sunset)
     for idx, i in enumerate(listed):
         session[str(idx)] = i
         print(idx)
@@ -47,26 +47,28 @@ def plot_png():
 # remeber to subtract one from len of session object for the time
 def create_figure(session_obj):
     fig = Figure(figsize=(10,3*len(session_obj)))
-    times = json.loads(session_obj['time'])
-    counter = 1
-    while counter < (len(session_obj) - 1):
-        for idx, i in enumerate(session_obj):
-            print(idx)
-            day = pd.read_json(session_obj[str(idx)], typ='series')
-            day = day.sort_index()
-            axis = fig.add_subplot(1, 1, 1)
-            axis.plot(times, day, label='Photovoltaic Energy Produced',
-                    color='orange', fillstyle='bottom')
-            axis.set_xlabel('Time', fontdict = {'fontsize' : 20})
-            axis.set_ylabel('Watt per Square Meter of Panels', fontdict = {'fontsize' : 20})
-            axis.legend(loc='upper left')
-            axis.set_title(f'Day {idx+1}', fontdict = {'fontsize' : 24}, loc= 'left')
-            for tick in axis.xaxis.get_major_ticks():
-                tick.label.set_fontsize(8) 
-                tick.label.set_rotation(65)
-            for tick in axis.yaxis.get_major_ticks():
-                tick.label.set_fontsize(22)
-            counter += 1
+    times = ["00:00", "00:15", "00:30", "00:45", "01:00", "01:15", "01:30", "01:45", "02:00", "02:15", "02:30", "02:45", "03:00", "03:15", "03:30", "03:45", "04:00", "04:15", "04:30", "04:45", "05:00", "05:15", "05:30", "05:45", "06:00", "06:15", "06:30", "06:45", "07:00", "07:15", "07:30", "07:45", "08:00", "08:15", "08:30", "08:45", "09:00", "09:15", "09:30", "09:45", "10:00", "10:15", "10:30", "10:45", "11:00", "11:15", "11:30", "11:45", "12:00", "12:15", "12:30", "12:45", "13:00", "13:15", "13:30", "13:45", "14:00", "14:15", "14:30", "14:45", "15:00", "15:15", "15:30", "15:45", "16:00", "16:15", "16:30", "16:45", "17:00", "17:15", "17:30", "17:45", "18:00", "18:15", "18:30", "18:45", "19:00", "19:15", "19:30", "19:45", "20:00", "20:15", "20:30", "20:45", "21:00", "21:15", "21:30", "21:45", "22:00", "22:15", "22:30", "22:45", "23:00", "23:15", "23:30", "23:45"]
+    # counter = 1
+    print(times)
+    # while counter < (len(session_obj)):
+    for idx, i in enumerate(session_obj):
+        len(session_obj)
+        print(idx)
+        day = pd.read_json(session_obj[str(idx)], typ='series')
+        day = day.sort_index()
+        axis = fig.add_subplot(1, 1, 1)
+        axis.plot(times, day, label='Photovoltaic Energy Produced',
+                color='orange', fillstyle='bottom')
+        axis.set_xlabel('Time', fontdict = {'fontsize' : 20})
+        axis.set_ylabel('Watt per Square Meter of Panels', fontdict = {'fontsize' : 20})
+        axis.legend(loc='upper left')
+        axis.set_title(f'Day {idx+1}', fontdict = {'fontsize' : 24}, loc= 'left')
+        for tick in axis.xaxis.get_major_ticks():
+            tick.label.set_fontsize(8) 
+            tick.label.set_rotation(65)
+        for tick in axis.yaxis.get_major_ticks():
+            tick.label.set_fontsize(22)
+        counter += 1
     return fig
 
 
