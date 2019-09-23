663b1782faec3b0cfb0801c906c70d8d6a3dec44
MarkEhler <ehlerm42@gmail.com>
2019-09-23 16:59:01 -0400

    last commit 9/23

--- app/api_calls.py
+++ app/api_calls.py
@@ -423,9 +423,6 @@ def process(final_data, days, sunrise, sunset):
     averages = []
     for day in dayz:
         day = final_data[final_data.Day == day]
-        for time in day[sunrise_minutes:sunset_minutes]:
-            if time < 9:
-                day[time] = random.randint(6,27)
         averages.append((daily_avg(day)))
         day = day['Output'].to_json()
         listed.append(day)
